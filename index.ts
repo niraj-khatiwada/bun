69b06ed4f47c463ada29bdcdefc747aa8a8f2e27
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-11-05 21:42:02 +0545

    bun server

--- index.ts
+++ index.ts
@@ -4,8 +4,16 @@ Bun.serve({
   port: 9001,
   development: true,
   fetch(req) {
-    console.log(new URL(req.url))
-    return new Response('<h1>Hello From Bun</h1>')
+    const url = new URL(req.url)
+    switch (url.pathname) {
+      case '/login':
+        return new Response('<h1>Hello From Login</h1>')
+
+      default: {
+        return new Response('<h1>Hello From Bun</h1>')
+      }
+    }
   },
 })
+
 console.log('Server started at port 9001')
